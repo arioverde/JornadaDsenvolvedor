8b5523aa3a14b0e18d2eb48785938369eb87f576
Andre Rio Verde <arioverde972@gmail.com>
2023-01-11 18:42:57 -0300

    Implementado lista ao invés de vetor, inserção da classe aluno

---
+++ Aula_3_Atividade_Matriz1/Aluno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_3_Atividade_Matriz1
+{
+    public class Aluno
+    {
+        public string? Nome { get; set; }    
+        public float Nota { get; set; }
+    }
+}
--- Aula_3_Atividade_Matriz1/MediaNotas.cs
+++ Aula_3_Atividade_Matriz1/MediaNotas.cs
@@ -8,39 +8,59 @@ namespace Aula_3_Atividade_Matriz1
 {
     public class MediaNotas
     {
-        double[] notasProvas = new double[5];
         public void Executar()
         {
             ExibeCabecalho();
-            SolicitaNotaProvas();
-            ExibeMediaNotas();
+            var alunos = SolicitaNotaProvas();
+            if (alunos.Count == 0)
+            {
+                Console.WriteLine("As notas não foram informadas.");
+                return;
+            }
+            ExibeMediaNotas(alunos);
         }
         private void ExibeCabecalho()
         {
             Console.WriteLine("*************************************************");
-            Console.WriteLine("***** Calculo da média de 5 notas de provas *****");
+            Console.WriteLine("****** Calculo da média de notas de provas ******");
             Console.WriteLine("*************************************************");
         }
-        private void SolicitaNotaProvas()
+        private List<Aluno> SolicitaNotaProvas()
         {
-            double nota;
-            for (int i = 0; i < 5; i++)
+            var alunos = new List<Aluno>();
+
+            while (true)
             {
-                Console.WriteLine($"Informe a nota da prova {i + 1}");
-                nota = Convert.ToDouble(Console.ReadLine());
-                notasProvas[i] = nota;
+                Console.WriteLine("1 - Cadastrar Notas");
+                Console.WriteLine("2 - Finalizar");
+
+                var opcao = Convert.ToInt16(Console.ReadLine());
+                if (opcao == 2)
+                    break;
+
+                var aluno = new Aluno();
+
+                Console.Write("Informe a NOTA: ");
+                aluno.Nota = Convert.ToInt16(Console.ReadLine());
+                Console.Write("Informe o NOME do aluno: ");
+                aluno.Nome = Console.ReadLine();
+
+                alunos.Add(aluno);
+
+                Console.Clear();
             }
+            return alunos;
         }
-        private void ExibeMediaNotas()
+        private void ExibeMediaNotas(List<Aluno> alunos)
         {
-            double somaNota = 0, media;
-            for (int i = 0; i < 5; i++)
+            var somaNotas = 0F;
+            foreach (var aluno in alunos)
             {
-                somaNota = somaNota + notasProvas[i];
+                Console.Write($"{aluno.Nome}: {aluno.Nota}" + Environment.NewLine);
+                somaNotas += aluno.Nota;
             }
-            
-            media = somaNota / 5;
-            Console.WriteLine($"A média das notas é {media}");
+
+            Console.WriteLine($"A média das notas é {somaNotas / alunos.Count}");
         }
     }
 }
